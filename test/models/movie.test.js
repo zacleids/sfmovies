b842a4ef378849db5b6aa92d4deba85139647c69
Zac Leids <zacleids@gmail.com>
2020-08-20 18:13:06 -0700

    fix lint errors

--- test/models/movie.test.js
+++ test/models/movie.test.js
@@ -4,19 +4,19 @@ const Movie = require('../../lib/models/movie');
 
 describe('movie model', () => {
 
-    describe('serialize', () => {
+  describe('serialize', () => {
 
-        it('includes all of the necessary fields', () => {
-            const movie = Movie.forge().serialize();
+    it('includes all of the necessary fields', () => {
+      const movie = Movie.forge().serialize();
 
-            expect(movie).to.have.all.keys([
-                'id',
-                'title',
-                'release_year',
-                'object'
-            ]);
-        });
-
+      expect(movie).to.have.all.keys([
+        'id',
+        'title',
+        'release_year',
+        'object'
+      ]);
     });
+
+  });
 
 });
