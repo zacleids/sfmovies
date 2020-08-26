699af164952109eaf7de558d04ec1792e5352ad5
Zac Leids <zacleids@gmail.com>
2020-08-26 15:08:10 -0700

    fix lint errors

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -4,16 +4,16 @@ const Controller = require('../../../../lib/plugins/features/movies/controller')
 
 describe('movie controller', () => {
 
-    describe('create', () => {
+  describe('create', () => {
 
-        it('creates a movie', async () => {
-            const payload = { title: 'WALL-E' };
+    it('creates a movie', async () => {
+      const payload = { title: 'WALL-E' };
 
-            const movie = await Controller.create(payload);
+      const movie = await Controller.create(payload);
 
-            expect(movie.get('title')).to.eql(payload.title);
-        });
-
+      expect(movie.get('title')).to.eql(payload.title);
     });
+
+  });
 
 });
--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -4,19 +4,19 @@ const Movies = require('../../../../lib/server');
 
 describe('movies integration', () => {
 
-    describe('create', () => {
+  describe('create', () => {
 
-        it('creates a movie', async () => {
-            const response = await Movies.inject({
-                url: '/movies',
-                method: 'POST',
-                payload: { title: 'Volver' }
-            });
+    it('creates a movie', async () => {
+      const response = await Movies.inject({
+        url: '/movies',
+        method: 'POST',
+        payload: { title: 'Volver' }
+      });
 
-            expect(response.statusCode).to.eql(200);
-            expect(response.result.object).to.eql('movie');
-        });
-
+      expect(response.statusCode).to.eql(200);
+      expect(response.result.object).to.eql('movie');
     });
+
+  });
 
 });
